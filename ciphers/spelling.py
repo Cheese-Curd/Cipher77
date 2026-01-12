ecb8477a245d6b93dab2b0b63211810150713287
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2026-01-11 23:58:12 -0500

    Nato Spelling

---
+++ ciphers/spelling.py
@@ -0,0 +1,54 @@
+import ciphers._global as globalVar
+
+NATO = (
+	"Alpha",
+	"Bravo",
+	"Charlie",
+	"Delta",
+	"Echo",
+	"Foxtrot",
+	"Golf",
+	"Hotel",
+	"India",
+	"Juliett",
+	"Kilo",
+	"Lima",
+	"Mike",
+	"November",
+	"Oscar",
+	"Papa",
+	"Quebec",
+	"Romeo",
+	"Sierra",
+	"Tango",
+	"Uniform",
+	"Victor"
+)
+
+NATO_REVERSE = {word.lower(): chr(ord('a') + i) for i, word in enumerate(NATO)}
+
+name = "Spelling Alphabet"
+desc = "Not really a cipher, but converts each letter into it's Vocal Form"
+
+def cipher(plain_text, encrypt):
+	totalStr = ""
+
+	if encrypt:
+		for char in plain_text:
+			base = globalVar.UPPER_OFFSET
+			if not char.isupper():
+				base = globalVar.LOWER_OFFSET
+
+			index = ord(char) - base
+
+			totalStr += f"{NATO[index]} "
+	else:
+		for word in plain_text.split():
+			char = NATO_REVERSE.get(word.lower())
+
+			if char:
+				totalStr += char
+			else:
+				totalStr += word
+
+	return totalStr

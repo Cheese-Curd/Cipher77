145bf29f5ccf89e26bb6caf1a4664e9a4768ee7a
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2026-01-11 23:31:15 -0500

    Caesar Cipher

---
+++ ciphers/caesar.py
@@ -0,0 +1,45 @@
+import ciphers._global as globalVar
+
+name = "Caesar Cipher"
+desc = "Standard Caesar Cipher, the one that started it all"
+
+def cipher(plain_text, encrypt):
+	offset = 0
+	while True:
+		try:
+			offset = int(input("Input Offset: "))
+			break
+		except:
+			print("Input valid offset.")
+
+	totalStr = ""
+	
+	if not encrypt:
+		offset = -offset
+
+	for char in plain_text:
+		if char == " ":
+			totalStr += " "
+			continue
+		if not char.isalpha():
+			totalStr += char
+			continue
+
+		if char == " ":
+			totalStr += " "
+			continue
+		if not char.isalpha():
+			totalStr += char
+			continue
+
+		base = globalVar.UPPER_OFFSET
+		if not char.isupper():
+			base = globalVar.LOWER_OFFSET
+
+
+		charOffset = offset
+		charOffset += (ord(char) - base)
+
+		totalStr += chr(base + charOffset)
+
+	return totalStr

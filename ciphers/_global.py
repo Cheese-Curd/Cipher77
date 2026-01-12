f11bb6c204d488911b0d4518d6c86f84bd809494
Eliana <51841108+Cheese-Curd@users.noreply.github.com>
2026-01-12 12:19:47 -0500

    Yes/No helper

--- ciphers/_global.py
+++ ciphers/_global.py
@@ -1,6 +1,6 @@
 UPPER_OFFSET = 65
 LOWER_OFFSET = 97
-TOTAL_CHAR   = 26TOTAL_CHAR   = 26
+TOTAL_CHAR   = 26
 
 def getOffset(char):
 	base = UPPER_OFFSET
@@ -8,3 +8,11 @@ def getOffset(char):
 		base = LOWER_OFFSET
 
 	return base
+
+def getYesNo(inputText=""):
+	while True:
+		response = input(f"{inputText} (Y/N): ").lower()
+		if response != "y" and response != "n":
+			print("Input valid choice.")
+		else:
+			return response == "y"

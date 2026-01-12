2c80b11eacc4b362a2d83ce99406f99dc416fca9
Eliana <51841108+Cheese-Curd@users.noreply.github.com>
2026-01-12 12:19:59 -0500

    A1Z26 Cipher

---
+++ ciphers/A1Z26.py
@@ -0,0 +1,32 @@
+import ciphers._global as globalVar
+
+name = "A1Z26 Cipher"
+desc = "The simpilist Cipher"
+
+def cipher(plain_text, encode):
+	seperator = input("Input Seperator: ")
+
+	totalStr = ""
+
+	if encode:
+		for char in plain_text:
+			if not char.isalpha():
+				continue
+
+			base = globalVar.getOffset(char)
+
+			if len(totalStr) > 0:
+				totalStr += seperator
+
+			totalStr += str((ord(char) - base) + 1)
+	else:
+		upper = globalVar.getYesNo("Uppercase?")
+
+		base = globalVar.UPPER_OFFSET
+		if upper == False:
+			base = globalVar.LOWER_OFFSET
+
+		for word in plain_text.split(seperator):
+			totalStr += chr((int(word) + base) - 1)
+	
+	return totalStr
